e30b2e5c852787a0c4ac7b988cf5ced4d011bfd5
Vasyl <vasyl@example.com>
2025-11-28 21:27:53 +0200

    logging

--- revision/app/main.py
+++ revision/app/main.py
@@ -1,4 +1,7 @@
+import logging
+
 from revision import utils
+from logger import logger
 
 
 def main():
@@ -6,6 +9,12 @@ def main():
     utils.is_number_bigger_than_threshold(15, 8)
     utils.is_number_bigger_than_threshold(30, )
     utils.is_number_bigger_than_threshold(5, )
+
+    logger.info('Hello here!!!')
+    # logger.warning('Hello warning!!!', extra={"user": 123})
+    # logger.error('Hello warning!!!', extra={"user": 123})
+    logger.debug('Hello warning!!!', extra={"user": 5555})
+    # logger.fatal('Hello warning!!!', extra={"user": 123})
 
 
 if __name__ == "__main__":
